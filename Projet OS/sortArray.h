b1fa04c25d63ecd0bc55805aeb08e090c3b8769c
Fire-Aspect <93075494+Fire-Aspect@users.noreply.github.com>
2022-09-27 21:29:06 +0200

    Add files via upload

---
+++ Projet OS/sortArray.h
@@ -0,0 +1,10 @@
+//
+// Created by antoi on 27/09/2022.
+//
+
+#ifndef PROJET_FORMULE_1_SORTARRAY_H
+#define PROJET_FORMULE_1_SORTARRAY_H
+
+int sortArray(int arr[], int length );
+
+#endif //PROJET_FORMULE_1_SORTARRAY_H
