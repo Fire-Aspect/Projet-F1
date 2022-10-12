862bc47ef21be32381aedcdda02ab07c48ef2f7c
Fire-Aspect <93075494+Fire-Aspect@users.noreply.github.com>
2022-10-12 12:03:19 +0200

    Delete Projet OS directory

--- Projet OS/timeGen.h
+++
@@ -1,9 +0,0 @@
-//
-// Created by antoi on 27/09/2022.
-//
-
-#ifndef PROJET_FORMULE_1_TIMEGEN_H
-#define PROJET_FORMULE_1_TIMEGEN_H
-
-int timeGenerator(int temps[]);
-#endif //PROJET_FORMULE_1_TIMEGEN_H
