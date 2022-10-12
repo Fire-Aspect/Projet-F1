58765236c5ec6a037662864e8bc86cf4eeffece0
Carlos Iglesias <92795126+iglesias1602@users.noreply.github.com>
2022-10-12 04:00:16 -0700

    Update showOutput.c
    Modifier pour afficher les millisecondes

--- Projet-F1-V3/Projet OS/showOutput.c
+++ Projet-F1-V3/Projet OS/showOutput.c
@@ -4,21 +4,16 @@
 
 int showOutput(Voiture v[], size_t len) {
 
-    char *rowC[] = {"Numero", "Secteur 1", "Secteur 2", "Secteur 3", "Temps"};
+    char *rowC[] = {"Numero", "Secteur 1", "Secteur 2", "Secteur 3", "Temps par tour"};
     
 
-    printf("%*s | %*s | %*s | %*s | %*s\n", -6, rowC[0], -15, rowC[1], -15, rowC[2], -15, rowC[3], -15, rowC[4]);
-    printf("%*s | %*s | %*s | %*s | %*s\n", -3, "------", 15, "---------------", 15, "---------------", 15, "---------------", -31, "-------------------------------");
+    printf("%*s | %*s | %*s | %*s | %*s\n", -6, rowC[0], -12, rowC[1], -12, rowC[2], -12, rowC[3], -15, rowC[4]);
+    printf("%*s | %*s | %*s | %*s | %*s\n", -3, "------", 12, "------------", 12, "------------", 12, "------------", -31, "-------------------------------");
     for (int i = 0; i < len; ++i) {
     
-    	if (v[i].tTour[0] == 1) {
     
-        	printf("%-6d | %d %-12s | %d %-12s | %d %-12s | %d %s %d %-11s\n", v[i].vId, v[i].s1, 		"secondes" , v[i].s2, "secondes" , v[i].s3, "secondes" , v[i].tTour[0], "minute", 			v[i].tTour[1], "secondes");
+        printf("%-6d | %-12.3f | %-12.3f | %-12.3f | %.0f%s%-11.3f\n", v[i].vId, v[i].s1, v[i].s2, v[i].s3, v[i].tTour[0], ".", v[i].tTour[1]);
         }
-        else {
-        	printf("%-6d | %d %-12s | %d %-12s | %d %-12s | %d %s %d %-11s\n", v[i].vId, v[i].s1, 		"secondes" , v[i].s2, "secondes" , v[i].s3, "secondes" , v[i].tTour[0], "minutes", 			v[i].tTour[1], "secondes");
-        }
-    }
-
+	printf("%s", "======================================================================================\n");
 
 }
