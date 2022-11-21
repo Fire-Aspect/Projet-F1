046ce5cfc8aeab9c784cf58f70fe1b1cef7368f1
Fire-Aspect <93075494+Fire-Aspect@users.noreply.github.com>
2022-11-21 12:41:46 +0100

    Delete Projet OS directory

--- Projet OS/showOutput.h
+++
@@ -1,9 +0,0 @@
-#ifndef PROJET_FORMULE_1_SHOWOUTPUT_H
-#define PROJET_FORMULE_1_SHOWOUTPUT_H
-
-#include <stdlib.h>
-#include "structVoiture.h"
-
-int showOutput(Voiture v[], size_t len);
-
-#endif //PROJET_FORMULE_1_SHOWOUTPUT_HPUT_H
