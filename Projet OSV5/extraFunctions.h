d1fbe3b307223d05c8244220ba5cbdc7dd83d20c
iglesias1602 <iglesias1602@gmail.com>
2022-11-28 13:58:39 +0100

    timer

--- Projet OSV5/extraFunctions.h
+++ Projet OSV5/extraFunctions.h
@@ -1,14 +1,9 @@
 #include<stdio.h>
 #include <unistd.h>
-
-    int timeLimiter() {
-
-        for (int i = 0; i < 60; i++) {
-            sleep(1);
-            printf("%d\n", i);
-
-        }
-
+int timeLimiter() {
+    for (int i = 0; i < 60; i++) {
+        sleep(1);
+        printf("%d\n", i);
     }
-
-    timeLimiter();
+    return 0;
+}
